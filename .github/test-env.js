24351d974791fb40a54615395470e09606fce66c
amocco44 <jccavatorta@gmail.com>
2025-04-08 19:16:20 +0200

    Create test-env.js

---
+++ .github/test-env.js
@@ -0,0 +1,3 @@
+console.log("Test des variables d'environnement:");
+console.log("SUPABASE_URL présente:", !!process.env.SUPABASE_URL);
+console.log("SUPABASE_SERVICE_ROLE_KEY présente:", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
